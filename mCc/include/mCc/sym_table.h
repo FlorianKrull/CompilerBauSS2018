7d4c658dad3705697d095b9dadc245ab2ab11d25
nga <ngapt.fit@gmail.com>
2018-04-24 15:15:31 +0200

    Initial symbol table

--- mCc/include/mCc/sym_table.h
+++ mCc/include/mCc/sym_table.h
@@ -7,19 +7,65 @@ extern "C" {
 
 #define SIZE 100
 
-struct Entry {
-	char *name;
+/* Declare the symbol table as a hashtable of list.
+ * More information: https://www.d.umn.edu/~rmaclin/cs5641/Notes/L15_SymbolTable.pdf
+ * When processing a scope S, the structure of the symbol table should look
+ * like this:
+ *
+ * f : void, 1
+ * a : int, 2
+ * x : char, 3 -> int, 2
+ * y : char, 3 -> float 2
+ * z : bool, 3
+ *
+ * The structure corresponding to the following code:
+ * void f(int a) {
+ * int x; float y;
+ * if (...) {
+ * char x, y;
+ * bool z;
+ * ...
+ * }
+ * }
+ * void main() {
+ * f(10);
+ * }
+ * */
+
+/* The basic element; storing declaration of a variables and its scope, respectively.
+ * It's also organized as linked list; in which next pointer hold the address of the
+ * next node. */
+struct mCc_st_item {
 	char *type;
-	int count;
-	struct Entry *next;
+	int scope;
+	struct mCc_st_item *next;
 };
 
-struct Table {
-	struct Entry entries[SIZE];
-	struct Table *parent;
+/* The variable bucket, in which a head pointer pointing to current node. */
+struct mCc_st_entry {
+	char *name;
+	char *id;
+	struct mCc_st_entry *head;
 };
 
-typedef struct Entry *EntryPtr;
+/* Hash pointer, stores an array of pointers to entries, and its level of scope. */
+struct mCc_st_table {
+	struct mCc_st_entry** entries;
+	int scope;
+};
+
+// Init and delete function
+struct mCc_st_item *mCc_st_new_item(const char* type, int scope, struct mCc_st_item* next);
+
+struct mCc_st_entry *mCc_st_new_entry(const char* name, const char* id);
+
+struct mCc_st_table *mCc_st_new_table();
+
+void mCc_st_delete_item(struct mCc_st_item* item);
+
+void mCc_st_delete_entry(struct mCc_st_entry* entry);
+
+void mCc_st_delete_table(struct mCc_st_table* table);
 
 #ifdef __cplusplus
 }
---
+++ mCc/src/sym_table.c
@@ -0,0 +1,78 @@
+#include "mCc/ast.h"
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* ---------------------------------------------------------------- Items */
+struct mCc_st_item *mCc_st_new_item(const char* type, int scope, struct mCc_st_item *next)
+{
+	assert(next);
+	struct mCc_st_item *item = malloc(sizeof(*item));
+	if (!item) {
+		return NULL;
+	}
+	item->type = strdup(type);
+	item->scope = scope;
+	item->next = next;
+	return item;
+}
+
+void mCc_st_delete_item(struct mCc_st_item* item)
+{
+	assert(item);
+	free(item->type);
+	if (!item->next) {
+		mCc_st_delete_item(item->next);
+	}
+	free(item);
+}
+
+/* ---------------------------------------------------------------- Entries */
+struct mCc_st_entry *mCc_st_new_entry(const char* name, const char* id, struct mCc_st_entry* head)
+{
+	struct mCc_st_entry *en = malloc(sizeof(*en));
+	if (!en) {
+		return NULL;
+	}
+	en->name = strdup(name);
+	en->id = strdup(id);
+	en->head = head;
+	return en;
+}
+
+void mCc_st_delete_entry(struct mCc_st_entry* entry)
+{
+	assert(entry);
+	free(entry->name);
+	free(entry->id);
+	if (!entry->head) {
+		mCc_st_delete_entry(entry->head);
+	}
+	free(entry);
+}
+
+/* ---------------------------------------------------------------- Tables */
+struct mCc_st_table *mCc_st_new_table()
+{
+	struct mCc_st_table *tab = malloc(sizeof(*tab));
+	if (!tab) {
+		return NULL;
+	}
+	tab->entries = calloc(SIZE, sizeof(**entries));
+	return tab;
+}
+
+void mCc_st_delete_table(struct mCc_st_table* table)
+{
+	assert(table);
+
+	for (int i = 0; i < SIZE; i++) {
+		mCc_st_entry* current_entry = table->entries[i];
+		if (!current_entry) {
+			mCc_st_delete_entry(current_entry);
+		}
+	}
+	free(table->entries);
+	free(table);
+}
